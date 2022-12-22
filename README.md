18151cf0877beecb9294bf95aca277f38a730b91
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2022-12-22 14:36:53 +0100

    Added command line arguments for custom resolution

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Portrait to landscape python
 This is a rewriten version of my bash script doing the same thing but a little more optimized.
 ## What does it do?
-This program takes a picture of any aspect ratio and makes it 16:9 in 4K resolution. Since this will use [waifu2x-ncnn-vulkan](https://github.com/nihui/waifu2x-ncnn-vulkan) it is recommended to use anime/manga styled images.
+This program takes a picture of any aspect ratio and change it to a custom resolution. Since this will use [waifu2x-ncnn-vulkan](https://github.com/nihui/waifu2x-ncnn-vulkan) it is recommended to use anime/manga styled images.
 ## How to use
 Firstly, use prepare-folders.py to create a couple of folders that the main script will use.
 ```
 python prepare-folders.py
 ```
-Then, place your images in the input directory. The images in this directory will get deleted. If you need to keep the original pictures then please back it up in another directory that isn't made by the script. Run the script if you are ready.
+Then, place your images in the input directory. And specify the resolution with -x and -y arguments. -x for the horizontal pixels and -y for the vertical pixels. The images in this directory will get deleted. If you need to keep the original pictures then please back it up in another directory that isn't made by the script. Run the script if you are ready.
 ```
-python main.py
+python main.py -x 3640 -y 2160 # For 4K
 ```
 The processed images will be in the output folder.
 ## Dependencies
@@ -17,5 +17,6 @@ The processed images will be in the output folder.
 - `imagemagick`
 - `waifu2x-ncnn-vulkan`
 - `oxipng`
+- `libjxl` (for cjxl)
 ## Caviats
 The script is made with usage on Linux in mind. Theorretically this should work in any other OS's as long as the dependencies are in your PATH.
--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-import os
 # For changing directory, listing files in directories and deleting files
-import subprocess
+import os
 # Run terminal commands
-from PIL import Image
+import subprocess
 # To get the width and height of a picture in pixels
-import shutil
+from PIL import Image
 # To move files around
+import shutil
+# To use all the cpu cores available for parallel computing
 from multiprocessing import Pool
 import multiprocessing
-# To use all the cpu cores available for parallel computing
+# Parse arguments
+import argparse
+
+# Parse aguments for later use
+parser = argparse.ArgumentParser(description="Python script for editing pictures to a specific resolution by adding blur to the sides.")
+parser.add_argument("-x", "--width", required=True, type=int, dest="cmd_width")
+parser.add_argument("-y", "--height", required=True, type=int, dest="cmd_height")
+args = parser.parse_args()
+
+cmd_width = args.cmd_width
+cmd_height = args.cmd_height
 
 # Making a function because it will be used twice and for practice
 def preprocess(to) :
     for item in os.listdir() :
         img = Image.open(item)
         width,height = img.size
-        if height < 2160 and width < 3840 :
+        if height < cmd_height and width < cmd_width :
             shutil.move(item, to)
 
 print("Starting the script.")
@@ -42,8 +53,13 @@ while os.listdir("1") != [] :
 print("I will now start to edit the pictures.")
 os.chdir("inbetween")
 
+# Convert the integers to strings so I can paste together a series of strings
+cmd_width = str(cmd_width)
+cmd_height = str(cmd_height)
+
 for item in os.listdir() :
-    subprocess.run(["convert","-size","3840x2160","xc:black",item,"-resize","3840x2160^","-blur","0x25","-gravity","center","-composite",item,"-geometry","3840x2160","-gravity","center","-composite","../inbetween2/" + item[:-4] + ".png"])
+    #subprocess.run(["convert","-size","3840x2160","xc:black",item,"-resize","3840x2160^","-blur","0x25","-gravity","center","-composite",item,"-geometry","3840x2160","-gravity","center","-composite","../inbetween2/" + item[:-4] + ".png"])
+    subprocess.run(["convert","-size",cmd_width+"x"+cmd_height,"xc:black",item,"-resize",cmd_width+"x"+cmd_height+"^","-blur","0x25","-gravity","center","-composite",item,"-geometry",cmd_width+"x"+cmd_height,"-gravity","center","-composite","../inbetween2/" + item[:-4] + ".png"])
     os.remove(item)
 os.chdir("..")
 
